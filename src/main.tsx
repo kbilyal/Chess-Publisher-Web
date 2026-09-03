4ffcfd90d1f6462b55f29f15454a2e7ce96b85bb
kbilyal <65768770+kbilyal@users.noreply.github.com>
2026-09-03 15:33:36 +0300

    Use product-owned organizer API auth bridge

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import './cloud/installStudioApiAuth';
+import './cloud/installOrganizerApiAuth';
 import App from './App.tsx';
 import {OnlineCloudProvider} from './cloud/OnlineCloudProvider';
 import './index.css';
