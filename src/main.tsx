c66572883036578f1979462d3460b613ddc55724
kbilyal <65768770+kbilyal@users.noreply.github.com>
2026-09-03 13:40:22 +0300

    Authenticate Studio backend API with Organizer Token

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import './cloud/installStudioApiAuth';
 import App from './App.tsx';
 import {CloudWorkspaceBootstrap} from './cloud/CloudWorkspaceBootstrap';
 import './index.css';
