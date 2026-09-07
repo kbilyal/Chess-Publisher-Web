94aa6c1f853e04b084cda6fb6a7a2058761342bf
kbilyal <65768770+kbilyal@users.noreply.github.com>
2026-09-07 21:56:55 +0300

    Load polished Companion login and tournament-list styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx';
 import {OnlineCloudProvider} from './cloud/OnlineCloudProvider';
 import './index.css';
 import './companion.css';
+import './companion-cloud.css';
 import './companion-publish-v2.css';
 import './companion-registration.css';
 import './companion-setup.css';
