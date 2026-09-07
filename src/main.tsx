93a50c09aaec37127f6393e3fb058cc4a93e5a82
kbilyal <65768770+kbilyal@users.noreply.github.com>
2026-09-07 21:24:24 +0300

    Load focused registration styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import {OnlineCloudProvider} from './cloud/OnlineCloudProvider';
 import './index.css';
 import './companion.css';
 import './companion-publish-v2.css';
+import './companion-registration.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
