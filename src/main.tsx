231a2396ba65676bec917a704fe99a45a2c3695d
kbilyal <65768770+kbilyal@users.noreply.github.com>
2026-09-13 17:35:20 +0300

    ui(web-registration): load roster number style

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,7 @@ import './companion.css';
 import './companion-cloud.css';
 import './companion-publish-v2.css';
 import './companion-registration.css';
+import './companion-roster-number.css';
 import './companion-setup.css';
 import './companion-conflict.css';
 import './mobile-native.css';
